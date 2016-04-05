f4a9b9b8479004b05903a3edcfb3c6a6d45b7e25
Rubén Fernández <rfernm08@estudiantes.unileon.es>
2016-04-05 12:42:50 +0200

    Realizados los metodos isSame

--- src/es/unileon/prg/date/Date.java
+++ src/es/unileon/prg/date/Date.java
@@ -42,7 +42,49 @@ public class Date{
 	boolean isSameDay(date other){
 		return(this.day == other.getDay());
 	}
+
 	boolean isSame(date other){
 		return((this.year == other.getYear()) && (this.month == other.getMonth()) && (this.day == other.getDay()))
 	}
+	
+	String monthName(){
+		String mes="";
+		switch(this.month){
+			case 1: 
+				mes = "Enero";
+				break;
+			case 2:
+				mes = "Febrero";
+				break;
+			case 3:
+				mes = "Marzo";
+				break;			
+			case 4:
+				mes = "Abril";
+				break;
+			case 5:
+				mes = "Mayo";
+				break;
+			case 6:
+				mes = "Junio";
+				break;
+			case 7: 
+				mes = "Julio";
+				break;
+			case 8:
+				mes = "Agosto";
+				break;
+			case 9:
+				mes = "Septiembre";
+				break;
+			case 10:
+				mes = "Octubre";
+				break;
+			case 11:
+				mes = "Noviembre";
+				break;
+			case 12:
+				mes = "Diciembre";
+				break;
+		}
 }
