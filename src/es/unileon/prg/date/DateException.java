36eaeb36643c4438c6fac1ae03230ad7ff1cf990
Rubén Fernández <rfernm08@estudiantes.unileon.es>
2016-04-07 17:40:15 +0200

    Date.java terminado

---
+++ src/es/unileon/prg/date/DateException.java
@@ -0,0 +1,7 @@
+package es.unileon.prg.date;
+
+public class DateException extends Exception{	
+	public String DateExcepcion(String msg){
+        	super(msg);
+	}
+}
---
+++ src/es/unileon/prg/date/MainDate.java
@@ -0,0 +1,9 @@
+package es.unileon.prg.date;
+
+public class MainDate{
+
+	public static void main(String[] args) {
+		Date date=new Date(2,4,2016);
+		System.out.println(Date.monthName());
+	}
+}
